005182aa7564023b45fd5aaca23196c645bd1650
Shivani <shivani.pawade121@gmail.com>
2024-02-20 22:32:34 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './auth/Login'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Login/>
+
     </>
      
   )
--- src/components/dashboard/Home/HomeComponent.jsx
+++ src/components/dashboard/Home/HomeComponent.jsx
@@ -1,11 +1,1 @@
-import React from 'react'
 
-const HomeComponent = () => {
-  return (
-    <div>
-      HomeComponent
-    </div>
-  )
-}
-
-export default HomeComponent
