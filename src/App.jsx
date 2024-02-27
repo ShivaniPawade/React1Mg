89b4e22c6e93690db4a45d522f7b354db695b62c
shailesh <gokhaleshail@gmail.com>
2024-02-27 21:10:20 +0530

    map component done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { Routes, Route , Outlet} from "react-router-dom";
 import HomeCards from "./components/dashboard/Home/HomeCards";
 import Layout from "./components/layout/Layout";
 import HomeComponent from "./components/dashboard/Home/HomeComponent";
+import MapComponent4 from "./components/maps/MapComponent4";
+import Employees from "./components/hrManagement/Employees";
 // import MapComponent from "./components/maps/MapComponent";
 // import MapComponent2 from "./components/maps/MapComponent2";
 // import MapComponent3 from "./components/maps/MapComponent3";
@@ -24,14 +26,15 @@ function App() {
           element={
             <Layout>
               <Outlet />
-              <HomeComponent/>
+              {/* <HomeComponent/> */}
+              <Employees/>
             </Layout>
           }
         >
           {/* Child routes rendered inside the Outlet */}
           {/* <Route path="/1MG Dashboard" element={<HomeComponent />} /> */}
           {/* <Route path="/map-live-view" element={<MapComponent2/>} /> */}
-          {/* <Route path="/map-live-view" element={<MapComponent3/>} /> */}
+          {/* <Route path="/map-live-view" element={<MapComponent4/>} /> */}
         
         </Route>
       </Routes>
